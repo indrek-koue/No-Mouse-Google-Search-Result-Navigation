fdb6ae67cebe336e8f285e4ae9f9efba49320c44
Indrek Kõue <indrek.koue@gmail.com>
2013-02-12 00:13:16 +0200

    onkeydown arroww + shift mokup

--- parse.js
+++ parse.js
@@ -1,42 +1,14 @@
-//37-40 arrows
-  // if (typeof event !== 'undefined') {
-    // keyPress = event.keyCode;
-  // }
-  // else if (e) {
-    // keyPress = e.which;
-  // }
-  
-var counter := 0;
+//40 - down
+//38 - up
 
+var counter = 0;
 document.onkeydown = function (e) {
-	
-	var wasOverrided := false;
-	if (typeof event !== 'undefined') {
-		
-		var results = document.getElementsByClassName("g");
-		
-		//find all links
-		var links;
-		for (var i = 0; i < results.length; i++){
-		links[i] = results[i].getElementsByTagName("a");
-		}
-		
-		
-		//down
-		if(event.keyCode == 40){
-			counter++;
-		}
-		//up
-		else if (event.keyCode == 38 && counter != 0){
-			counter--;
-		}
-	
-		results[counter].style.background = "rgba(255,0,0,0.1) !important";
-	
-		wasOverrided = true;
-	}
-  
-//your_array.push(String.fromCharCode(keyPress));
-// alert(String.fromCharCode(keyPress));
-	return !wasOverrided;   // Prevents the default action
+
+    if (event.shiftKey && event.keyCode == 40) {
+        //alert(String.fromCharCode(event.keyCode));
+        alert(++counter);
+        var eles = $("div.kaka");
+        eles.toggleClass("g");
+    }
+
 };
