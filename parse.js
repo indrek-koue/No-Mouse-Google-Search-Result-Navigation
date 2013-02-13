73e321eb2911f79672164a6877e5b14af925e85d
Indrek Koue <indrek.koue@apptanker.com>
2013-02-13 17:49:48 +0200

    select logic

--- parse.js
+++ parse.js
@@ -1,14 +1,26 @@
-//40 - down
-//38 - up
+//alert(String.fromCharCode(event.keyCode));
 
-var counter = 0;
+var pointer = 0;
 document.onkeydown = function (e) {
 
-    if (event.shiftKey && event.keyCode == 40) {
-        //alert(String.fromCharCode(event.keyCode));
-        alert(++counter);
+    if (event.shiftKey) {
         var eles = $("div.kaka");
-        eles.toggleClass("g");
-    }
+        var elementsCount = eles.length;
 
+        if (event.keyCode == 40) {
+            //down          
+            if (pointer != elementsCount - 1) {
+                eles.eq(pointer).toggleClass("g");
+                pointer++;
+                eles.eq(pointer).toggleClass("g");
+            }
+        } else if (event.keyCode == 38) {
+            //up
+            if (pointer != 0) {
+                eles.eq(pointer).toggleClass("g");
+                pointer--;
+                eles.eq(pointer).toggleClass("g");
+            }
+        }
+    }
 };
