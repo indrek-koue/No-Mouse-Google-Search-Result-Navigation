2a95b73846d6cca66316713cb6a85363690129b0
Indrek Kõue <indrek.koue@gmail.com>
2013-02-17 03:43:53 +0200

    event broken

--- parse.js
+++ parse.js
@@ -1,26 +1,38 @@
 //alert(String.fromCharCode(event.keyCode));
 
-var pointer = 0;
+var highlightCssClass = "nomouse-chrome-extension-highlight";
+var pointer = -1;
+var selectedUrl = "";
+
+var KEY_ARROW_DOWN = 40;
+var KEY_ARROW_UP = 38;
+var KEY_SPECIAL_ENTER = 13;
+
 document.onkeydown = function (e) {
 
-    if (event.shiftKey) {
-        var eles = $("div.kaka");
+    if (e.shiftKey) {
+		//alert("shift");
+        var eles = $("li.g");
         var elementsCount = eles.length;
+		
+		alert(eles.length);
 
-        if (event.keyCode == 40) {
-            //down          
-            if (pointer != elementsCount - 1) {
-                eles.eq(pointer).toggleClass("g");
-                pointer++;
-                eles.eq(pointer).toggleClass("g");
+        if (e.keyCode == KEY_SPECIAL_ENTER && selectedUrl != "") {
+            window.location = selectedUrl;
+        } else if (e.keyCode == KEY_ARROW_DOWN || e.keyCode == KEY_ARROW_UP) {
+            //turn off higlight for previous item
+            eles.eq(pointer).removeClass(highlightCssClass);
+			
+            //highlight current item
+            if (e.keyCode == KEY_ARROW_DOWN && pointer != elementsCount - 1) {
+                eles.eq(++pointer).addClass(highlightCssClass);
+            } else if (e.keyCode == KEY_ARROW_UP && pointer != 0) {
+                eles.eq(--pointer).addClass(highlightCssClass);
             }
-        } else if (event.keyCode == 38) {
-            //up
-            if (pointer != 0) {
-                eles.eq(pointer).toggleClass("g");
-                pointer--;
-                eles.eq(pointer).toggleClass("g");
-            }
-        }
-    }
+       //get selected section url
+        selectedUrl = eles.eq(pointer).find("a[href]:first").attr("href");
+        alert(urlString);
+  
+		}//key
+    }//shift
 };
