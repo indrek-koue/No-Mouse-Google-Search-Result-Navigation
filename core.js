aee0b9151148ae07eb07feded6dc5abfd3c1f7ad
Indrek Koue <indrek.koue@apptanker.com>
2013-02-19 16:01:41 +0200

    #1

--- core.js
+++ core.js
@@ -2,36 +2,39 @@
 
 var highlightCssClass = "nomouse-chrome-extension-highlight";
 var pointer = -1;
-var selectedUrl = "";
+var selectedUrl = STRING_EMPTY;
 
 var KEY_ARROW_DOWN = 40;
+var KEY_ARROW_RIGHT = 39;
 var KEY_ARROW_UP = 38;
 var KEY_SPECIAL_ENTER = 13;
 
+//var CSS_HIGHLIGHT_KEY = "background-color";
+//var CSS_HIGHLIGHT_VALUE = "rgba(255, 0, 0, 0.4) !important";
+
+var STRING_EMPTY = "";
 document.onkeydown = function (e) {
 
     if (e.shiftKey) {
-		//alert("shift");
         var eles = $("li.g");
         var elementsCount = eles.length;
-		
-		//alert(eles.length);
 
-        if (e.keyCode == KEY_SPECIAL_ENTER && selectedUrl != "") {
-            window.location = selectedUrl;
-        } else if (e.keyCode == KEY_ARROW_DOWN || e.keyCode == KEY_ARROW_UP) {
+        if (e.keyCode == KEY_ARROW_RIGHT && selectedUrl != STRING_EMPTY) {
+		//alert("goto: " + selectedUrl);
+		window.location = selectedUrl;
+		} else if (e.keyCode == KEY_ARROW_DOWN || e.keyCode == KEY_ARROW_UP) {
             //turn off higlight for previous item
             eles.eq(pointer).removeClass(highlightCssClass);
-			
-            //highlight current item
-            if (e.keyCode == KEY_ARROW_DOWN && pointer != elementsCount - 1) {
+						
+            //highlight current item, go out of index and hide highlight 
+			//when user navigates over the limit
+            if (e.keyCode == KEY_ARROW_DOWN && pointer != elementsCount) {
                 eles.eq(++pointer).addClass(highlightCssClass);
-            } else if (e.keyCode == KEY_ARROW_UP && pointer != 0) {
+            } else if (e.keyCode == KEY_ARROW_UP && pointer != -1) {
                 eles.eq(--pointer).addClass(highlightCssClass);
             }
-       //get selected section url
+      
         selectedUrl = eles.eq(pointer).find("a[href]:first").attr("href");
-        alert(urlString);
   
 		}//key
     }//shift
